db1ca8bd772ca6b14001868bc4720674e7ae4a94
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2022-11-28 20:45:26 +0100

    Finish task 1.2

--- lab_1/task_2/client.py
+++ lab_1/task_2/client.py
@@ -1,6 +1,6 @@
 # Laboratorium 1 Zadanie 1.2 Klient w Pythonie
 # Autorzy: Mateusz Brzozowski, Bartłomiej Krawczyk, Jakub Marcowski, Aleksandra Sypuła
-# Data ukończenia: 27.11.2022
+# Data ukończenia: 28.11.2022
 
 import socket
 import sys
@@ -8,14 +8,9 @@ from typing import List, Tuple
 import random
 import string
 
-# Klient wysyła a serwer odbiera datagramy o przyrastającej wielkości. 
-# Sprawdzić jaki był maksymalny rozmiar wysłanego (przyjętego) datagramu. 
+# Klient wysyła a serwer odbiera datagramy o przyrastającej wielkości.
+# Sprawdzić jaki był maksymalny rozmiar wysłanego (przyjętego) datagramu.
 # Ustalić z dokładnością do jednego bajta jak duży datagram jest obsługiwany.
-
-DATA_GRAM_ADD = 1 # # how much to add to next datagrams
-DATA_GRAM_MULT = 1.1 # how many times bigger are next datagrams
-DATA_GRAM_START_LEN = 65506
-MSG_TOO_LONG=False
 
 
 def get_random_string(length: int) -> str:
@@ -47,32 +42,40 @@ def connect_with_server(s: socket.socket, host: str, port: int) -> None:
         )
 
 
-def send_text(s: socket.socket, text: str) -> None:
-    print('Sending data')
+def send_text(s: socket.socket, length: int) -> bool:
+    print(f'Trying to send data with length: {length}')
     try:
-        s.send(text.encode('ascii'))
+        s.send(get_random_string(length).encode('ascii'))
     except socket.error as exception:
         print(f'Exception while sending data: {exception}')
-        global MSG_TOO_LONG
-        MSG_TOO_LONG=True
+        return False
     except UnicodeEncodeError as exception:
         print(f'Exception while encoding text to bytes: {exception}')
+        return False
+    return True
 
 
-def prepare_socket_and_start_sending_data(host: str, port: int) -> None:
+def prepare_socket_and_start_sending_data(host: str, port: int) -> int:
 
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         connect_with_server(s, host, port)
-        datagram_len = DATA_GRAM_START_LEN
-        while work():
-            send_text(s, get_random_string(int(datagram_len)))
-            datagram_len += DATA_GRAM_ADD
-            # datagram_len *= DATA_GRAM_MULT
+        data_length: int = 1
+        jump: int = 2
+        JUMP_MULTIPLIER: float = 2.0
+        while work(jump):
+            if send_text(s, data_length + jump):
+                data_length += jump
+                jump = int(jump * JUMP_MULTIPLIER)
+            else:
+                jump = int(jump / JUMP_MULTIPLIER)
+        return data_length
 
-def work() -> bool:
-    if MSG_TOO_LONG:
+
+def work(jump_size: int) -> bool:
+    if jump_size == 0:
         return False
     return True
+
 
 def main(args: List[str]) -> None:
     try:
@@ -85,7 +88,8 @@ def main(args: List[str]) -> None:
         return
 
     try:
-        prepare_socket_and_start_sending_data(host, port)
+        max_data_length = prepare_socket_and_start_sending_data(host, port)
+        print(f'Max data length: {max_data_length}')
     except socket.error as exception:
         print(f'Caught exception: {exception}')
 
