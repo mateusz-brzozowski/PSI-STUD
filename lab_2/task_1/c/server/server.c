706dde4b2f724ea538aeecabe164ce2c9e0717c7
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2022-12-01 20:07:47 +0100

    Finish task 2.1

--- lab_2/task_1/c/server/server.c
+++ lab_2/task_1/c/server/server.c
@@ -13,7 +13,7 @@
 #include <unistd.h>
 
 #define WORK 1
-#define BUFSIZE 1024
+#define BUFSIZE 10
 
 struct arguments {
     struct hostent *host_address;
@@ -28,9 +28,9 @@ int parse_argument(int argc, char *argv[]) {
     if (argc < 2) {
         port = htons(8000);
     } else {
-        if (port=atoi(argv[1]))
-            port=htons(port);
-        else{
+        if (port = atoi(argv[1]))
+            port = htons(port);
+        else {
             perror("Error, not able to parse provided arguments.");
             exit(1);
         }
@@ -42,53 +42,87 @@ int parse_argument(int argc, char *argv[]) {
     return port;
 }
 
+void receive_message(int socket_file_descriptor) {
+    struct sockaddr_in client_address;
+    socklen_t client_address_length = sizeof client_address;
+    char buffer[BUFSIZE + 1];
+    int max_message_buffer_size = 100000;
+    char message_buffer[max_message_buffer_size + 1];
+    char *client_address_string;
+    int message_socket_file_descriptor;
+    int bytes_read;
+    int already_read = 0;
+
+    buffer[BUFSIZE] = '\0';
+    memset(&message_buffer, 0, max_message_buffer_size);
+    memset(&client_address, 0, client_address_length);
+
+    message_socket_file_descriptor =
+        accept(socket_file_descriptor, (struct sockaddr *)&client_address, &client_address_length);
+    if (message_socket_file_descriptor == -1) {
+        perror("accept");
+        exit(4);
+    }
+
+    client_address_string = inet_ntoa(client_address.sin_addr);
+    if (client_address_string != 0) {
+        printf("Client address: %s:%d\n", client_address_string, ntohs(client_address.sin_port));
+    }
+
+    do {
+        bytes_read = recv(message_socket_file_descriptor, buffer, BUFSIZE, 0);
+        if (bytes_read == -1) {
+            perror("Exception while receiving stream packet");
+        } else {
+            buffer[bytes_read] = '\0';
+
+            if (already_read + bytes_read > max_message_buffer_size) {
+                printf("Didn't expect such a long message!");
+                printf("Part of long message: %s", message_buffer);
+                memset(&message_buffer, 0, max_message_buffer_size);
+                already_read = 0;
+            }
+            memcpy(message_buffer + already_read, buffer, bytes_read + 1);
+            already_read += bytes_read;
+
+            printf("%s\n", buffer);
+
+            fflush(stdout);
+        }
+    } while (bytes_read > 0);
+
+    printf("Message received: %s\n", message_buffer);
+
+    close(message_socket_file_descriptor);
+}
+
 int main(int argc, char *argv[]) {
     int socket_file_descriptor;
     struct sockaddr_in server_address;
-    struct sockaddr_in client_address;
-    char buffer[BUFSIZE + 1];
-    socklen_t client_address_length = sizeof client_address;
-    char *client_address_string;
 
-    buffer[BUFSIZE] = '\0';
-
-    if ((socket_file_descriptor = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
-        perror("Error while opening datagram socket");
-        exit(1);
+    if ((socket_file_descriptor = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("Error while opening stream socket");
+        exit(2);
     }
 
     memset(&server_address, '\0', sizeof(server_address));
+
     server_address.sin_family = AF_INET;
     server_address.sin_addr.s_addr = INADDR_ANY;
     server_address.sin_port = parse_argument(argc, argv);
 
     if (bind(socket_file_descriptor, (struct sockaddr *)&server_address, sizeof(server_address)) == -1) {
-        perror("binding datagram socket");
-        exit(2);
+        perror("binding stream socket");
+        exit(3);
     }
 
-    memset(&client_address, 0, client_address_length);
+    listen(socket_file_descriptor, 10);
 
     while (WORK) {
-        memset(&client_address, 0, client_address_length);
-
-        int bytes_read = recvfrom(socket_file_descriptor, buffer, BUFSIZE, 0, (struct sockaddr *)&client_address,
-                                  &client_address_length);
-
-        if (bytes_read == -1) {
-            perror("Exception while receiving datagram packet");
-        } else {
-            buffer[bytes_read] = '\0';
-
-            printf("Message received: %s\n", buffer);
-            client_address_string = inet_ntoa(client_address.sin_addr);
-            if (client_address_string != 0) {
-                printf("Client address: %s:%d\n", client_address_string, ntohs(client_address.sin_port));
-            }
-            fflush(stdout);
-        }
+        receive_message(socket_file_descriptor);
     }
 
     close(socket_file_descriptor);
+
     return 0;
 }
