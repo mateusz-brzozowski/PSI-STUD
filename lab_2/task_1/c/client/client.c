07c40e95ef79615b58b5393d41f0515d75877023
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2022-12-01 16:50:20 +0100

    Add c files from previous task

---
+++ lab_2/task_1/c/client/client.c
@@ -0,0 +1,111 @@
+// Laboratorium 1 Zadanie 1.1 Klient w C
+// Autorzy: Mateusz Brzozowski, Bartłomiej Krawczyk, Jakub Marcowski, Aleksandra Sypuła
+// Data ukończenia: 28.11.2022
+
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define ALPHABET "abcdefghijklmnopqrstuvwxyz"
+#define DATA_GRAM_LENGTH 60
+#define DATA_GRAM_NUMBER 10
+
+struct arguments {
+    struct hostent *host_address;
+    u_int16_t port;
+};
+
+typedef struct arguments Arguments;
+
+void get_random_string(char *buffer, size_t buffer_length) {
+    size_t n;
+    static char charset[] = ALPHABET;
+    if (buffer_length) {
+        if (buffer) {
+            for (n = 0; n < buffer_length; ++n) {
+                int key = rand() % (int)(sizeof(charset) - 1);
+                buffer[n] = charset[key];
+            }
+        }
+    }
+}
+
+void parse_arguments(int argc, char *argv[], Arguments *arguments) {
+    char *host;
+    u_int16_t port;
+
+    if (argc < 3) {
+        host = "localhost";
+        port = htons(8000);
+    } else {
+        host = argv[1];
+        if (port = atoi(argv[2]))
+            port = htons(port);
+        else {
+            perror("Error, not able to parse provided arguments.");
+            exit(1);
+        }
+    }
+
+    struct hostent *host_info;
+    host_info = gethostbyname(host);
+
+    if (host_info == (struct hostent *)0) {
+        fprintf(stderr, "%s: unknown host\n", host);
+        exit(2);
+    }
+
+    printf("Will send to %s:%d\n", host, ntohs(port));
+
+    arguments->host_address = host_info;
+    arguments->port = port;
+}
+
+int main(int argc, char *argv[]) {
+    int sock;
+    struct sockaddr_in name;
+    Arguments arguments;
+    u_int8_t i;
+
+    parse_arguments(argc, argv, &arguments);
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1) {
+        perror("opening datagram socket");
+        exit(1);
+    }
+
+    memcpy((char *)&name.sin_addr, (char *)arguments.host_address->h_addr, arguments.host_address->h_length);
+
+    name.sin_family = AF_INET;
+    name.sin_port = arguments.port;
+
+    if (connect(sock, (struct sockaddr *)&name, sizeof(name)) == -1) {
+        perror("Connect");
+        exit(3);
+    }
+
+    char buffer[DATA_GRAM_LENGTH + 1];
+    buffer[DATA_GRAM_LENGTH] = '\0';
+
+    for (i = 0; i < DATA_GRAM_NUMBER; ++i) {
+        get_random_string(buffer, DATA_GRAM_LENGTH);
+        printf("Sending %s\n", buffer);
+
+        if (send(sock, buffer, DATA_GRAM_LENGTH, 0) == -1) {
+            perror("sending datagram message");
+            exit(4);
+        }
+    }
+
+    printf("Client finished.\n");
+    fflush(stdout);
+
+    close(sock);
+    return 0;
+}
---
+++ lab_2/task_1/c/server/server.c
@@ -0,0 +1,94 @@
+// Laboratorium 1 Zadanie 1.1 Serwer w C
+// Autorzy: Mateusz Brzozowski, Bartłomiej Krawczyk, Jakub Marcowski, Aleksandra Sypuła
+// Data ukończenia: 27.11.2022
+
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define WORK 1
+#define BUFSIZE 1024
+
+struct arguments {
+    struct hostent *host_address;
+    u_int16_t port;
+};
+
+typedef struct arguments Arguments;
+
+int parse_argument(int argc, char *argv[]) {
+    u_int16_t port;
+
+    if (argc < 2) {
+        port = htons(8000);
+    } else {
+        if (port=atoi(argv[1]))
+            port=htons(port);
+        else{
+            perror("Error, not able to parse provided arguments.");
+            exit(1);
+        }
+    }
+
+    printf("Will listen on 0.0.0.0:%d\n", ntohs(port));
+    fflush(stdout);
+
+    return port;
+}
+
+int main(int argc, char *argv[]) {
+    int socket_file_descriptor;
+    struct sockaddr_in server_address;
+    struct sockaddr_in client_address;
+    char buffer[BUFSIZE + 1];
+    socklen_t client_address_length = sizeof client_address;
+    char *client_address_string;
+
+    buffer[BUFSIZE] = '\0';
+
+    if ((socket_file_descriptor = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+        perror("Error while opening datagram socket");
+        exit(1);
+    }
+
+    memset(&server_address, '\0', sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = parse_argument(argc, argv);
+
+    if (bind(socket_file_descriptor, (struct sockaddr *)&server_address, sizeof(server_address)) == -1) {
+        perror("binding datagram socket");
+        exit(2);
+    }
+
+    memset(&client_address, 0, client_address_length);
+
+    while (WORK) {
+        memset(&client_address, 0, client_address_length);
+
+        int bytes_read = recvfrom(socket_file_descriptor, buffer, BUFSIZE, 0, (struct sockaddr *)&client_address,
+                                  &client_address_length);
+
+        if (bytes_read == -1) {
+            perror("Exception while receiving datagram packet");
+        } else {
+            buffer[bytes_read] = '\0';
+
+            printf("Message received: %s\n", buffer);
+            client_address_string = inet_ntoa(client_address.sin_addr);
+            if (client_address_string != 0) {
+                printf("Client address: %s:%d\n", client_address_string, ntohs(client_address.sin_port));
+            }
+            fflush(stdout);
+        }
+    }
+
+    close(socket_file_descriptor);
+    return 0;
+}
