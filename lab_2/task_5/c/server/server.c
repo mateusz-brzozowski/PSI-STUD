3f9ad839dc4225381e8a98b1b346805d218f80c4
Jakub Mateusz Marcowski <01158831@pw.edu.pl>
2022-12-06 15:35:40 +0100

    Remove unused code

--- lab_2/task_5/c/server/server.c
+++ lab_2/task_5/c/server/server.c
@@ -42,62 +42,6 @@ int parse_argument(int argc, char *argv[]) {
     return port;
 }
 
-void receive_message(int socket_file_descriptor) {
-    struct sockaddr_in client_address;
-    socklen_t client_address_length = sizeof client_address;
-    char buffer[BUFSIZE + 1];
-    int max_message_buffer_size = 100000;
-    char message_buffer[max_message_buffer_size + 1];
-    char *client_address_string;
-    int message_socket_file_descriptor;
-    int bytes_read;
-    int already_read = 0;
-
-    buffer[BUFSIZE] = '\0';
-    memset(&message_buffer, 0, max_message_buffer_size);
-    memset(&client_address, 0, client_address_length);
-
-    message_socket_file_descriptor =
-        accept(socket_file_descriptor, (struct sockaddr *)&client_address, &client_address_length);
-    if (message_socket_file_descriptor == -1) {
-        perror("accept");
-        exit(4);
-    }
-
-    client_address_string = inet_ntoa(client_address.sin_addr);
-    if (client_address_string != 0) {
-        printf("Client address: %s:%d\n", client_address_string, ntohs(client_address.sin_port));
-    }
-
-    do {
-        bytes_read = recv(message_socket_file_descriptor, buffer, BUFSIZE, 0);
-        if (bytes_read == -1) {
-            perror("Exception while receiving stream packet");
-        } else {
-            buffer[bytes_read] = '\0';
-
-            if (already_read + bytes_read > max_message_buffer_size) {
-                printf("Didn't expect such a long message!");
-                printf("Part of long message: %s", message_buffer);
-                memset(&message_buffer, 0, max_message_buffer_size);
-                already_read = 0;
-            }
-            memcpy(message_buffer + already_read, buffer, bytes_read + 1);
-            already_read += bytes_read;
-
-            printf("%s\n", buffer);
-
-            fflush(stdout);
-        }
-    } while (bytes_read > 0);
-
-    printf("Message received: %s\n", message_buffer);
-
-    close(message_socket_file_descriptor);
-}
-
-/* makes use of fork() and wait() to handle multiple clients
-prints out what thread the message was received on */
 void receive_message_concurrent(int socket_file_descriptor) {
     struct sockaddr_in client_address;
     socklen_t client_address_length = sizeof client_address;
@@ -165,7 +109,6 @@ int main(int argc, char *argv[]) {
     listen(socket_file_descriptor, 10);
 
     while (WORK) {
-        // receive_message(socket_file_descriptor);
         receive_message_concurrent(socket_file_descriptor);
     }
 
