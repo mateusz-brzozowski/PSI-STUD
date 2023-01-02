12c4afac129c2a6693d5747a5b86f106ecb7d2de
Jakub Mateusz Marcowski <01158831@pw.edu.pl>
2023-01-02 23:55:02 +0100

    Update session.py

--- project/src/session.py
+++ project/src/session.py
@@ -2,7 +2,7 @@ import sys
 from typing import List, Optional
 
 from database import Database
-from packet import Packet
+from packet import Packet, packet_type_client, packet_type_server
 
 
 class SessionManager:
@@ -27,6 +27,7 @@ class SessionManager:
     w bazie danych
     """
 
+    state: int
     session_id: int
     session_key: str
     public_key: str
@@ -38,77 +39,113 @@ class SessionManager:
         self.host = host
         self.port = port
         self.database = database
+        self.state = session_manager_states["INIT"]
 
     def handle(self, packet: Packet) -> Optional[Packet]:
         packet_type = packet.content()[:3]
-        print(f"Typ datagramu: {packet_type!r}")
-        if packet_type == b"000":
-            # potwierdzenie otwarcia sesji
-            checksum = packet.content()[3:11]
-            print("Otrzymano potwierdzenie otwarcia sesji")
-            print(f"Suma kontrolna datagramu: {checksum!r}")
-        elif packet_type == b"001":
-            # uzgodnienie klucza symetrycznego
-            checksum = packet.content()[3:19]
-            server_public_key_B = packet.content()[19:83]
-            print("Uzgodniono klucz sesyjny")
-            print(f"Suma kontrolna datagramu: {checksum!r}")
-            print(f"Klucz publiczny serwera (B): {server_public_key_B!r}")
-        elif packet_type == b"010":
-            # potwierdzenie odebrania informacji o sesji
-            print("Potwierdzeno odebrania informacji o sesji")
-            checksum = packet.content()[3:11]
-            print("Otrzymano potwierdzenie otwarcia sesji")
-            print(f"Suma kontrolna datagramu: {checksum!r}")
-        elif packet_type == b"011":
-            # potwierdzenie odbioru paczki danych
-            print("Potwierdzono odbiór paczki danych")
-            checksum = packet.content()[3:19]
-            datagram_num = packet.content()[19:35]
-            print("Otrzymano potwierdzenie otwarcia sesji")
-            print(f"Suma kontrolna datagramu: {checksum!r}")
-            print(f"Numer datagramu: {datagram_num!r}")
-        elif packet_type == b"100":
-            # przesłanie kodu błędu
-            print("Przesłano kod błędu")
-            checksum = packet.content()[3:11]
-            print("Otrzymano potwierdzenie otwarcia sesji")
-            print(f"Suma kontrolna datagramu: {checksum!r}")
-        elif packet_type == b"101":
-            # zamknięcie sesji
-            print("Zamknięto sesję")
-            checksum = packet.content()[3:11]
-            print("Otrzymano potwierdzenie otwarcia sesji")
-            print(f"Suma kontrolna datagramu: {checksum!r}")
+        if packet_type == packet_type_client["initial"]:
+            self.handle_init()
+        elif packet_type == packet_type_client["session_data"]:
+            self.handle_session_data(packet)
+        elif packet_type == packet_type_client["declaration"]:
+            self.handle_declaration()
+        elif packet_type == packet_type_client["send"]:
+            self.handle_send(packet)
+        elif packet_type == packet_type_client["close"]:
+            self.handle_close()
         else:
-            raise ValueError(f"Unrecognized packet: {packet_type!r}")
-        print()
-        return packet
+            return Packet(packet_type_server["error"])
+
+    def handle_init(self) -> Packet:
+        """
+        Metoda pomocnicza służąca
+        potwierdzeniu otwarcia sesji
+        """
+        if self.state != session_manager_states["INIT"]:
+            self.state = session_manager_states["ERROR"]
+            return Packet(packet_type_server["error"])
+        self.state = session_manager_states["SYMMETRIC_KEY_NEGOTIATION"]
+        return Packet(packet_type_server["initial"])
+
+    def handle_session_data(self, packet: Packet) -> Packet:
+        """
+        Metoda pomocnicza służąca
+        uzgodnieniu klucza symetrycznego
+        """
+        if self.state != session_manager_states["SYMMETRIC_KEY_NEGOTIATION"]:
+            self.state = session_manager_states["ERROR"]
+            return Packet(packet_type_server["error"])
+        server_public_key_B = packet.content()[3:67]
+        self.state = session_manager_states["SESSION_CONFIRMATION"]
+        return Packet(packet_type_server["session_data"] + server_public_key_B)
+
+    def handle_declaration(self) -> Packet:
+        """
+        Metoda pomocznicza służąca
+        potwierdzeniu odebrania informacji o sesji
+        """
+        if self.state != session_manager_states["SESSION_CONFIRMATION"]:
+            self.state = session_manager_states["ERROR"]
+            return Packet(packet_type_server["error"])
+        self.state = session_manager_states["DATA_TRANSFER"]
+        return Packet(packet_type_server["acknowledge"])
+
+    def handle_send(self, packet: Packet) -> Packet:
+        """
+        Metoda pomocnicza służąca
+        potwierdzeniu odbioru paczki danych
+        """
+        if self.state != session_manager_states["DATA_TRANSFER"]:
+            self.state = session_manager_states["ERROR"]
+            return Packet(packet_type_server["error"])
+        datagram_num = packet.content()[3:19]
+        self.state = session_manager_states["SESSION_CLOSING"]
+        return Packet(packet_type_server["receive"] + datagram_num)
+
+    def handle_close(self) -> Packet:
+        """
+        Metoda pomocnicza służąca
+        potwierdzeniu zamknięcia sesji
+        """
+        if self.state != session_manager_states["SESSION_CLOSING"]:
+            self.state = session_manager_states["ERROR"]
+            return Packet(packet_type_server["error"])
+        self.state = session_manager_states["INIT"]
+        return Packet(packet_type_server["close"])
+
+
+session_manager_states = {
+    "INIT": 0,
+    "SYMMETRIC_KEY_NEGOTIATION": 1,
+    "SESSION_CONFIRMATION": 2,
+    "DATA_TRANSFER": 3,
+    "SESSION_CLOSING": 4,
+    "ERROR": 5
+}
 
 
 def main(args: List[str]) -> None:
-    session_manager = SessionManager("localhost", 8080)
-    session_manager.handle(Packet(b"000" + b"11000110"))  # type, checksum
+    db = Database()
+    session_manager = SessionManager("localhost", 8080, db)
+    session_manager.handle(Packet(b"000"))  # type
     session_manager.handle(
         Packet(
             b"001"  # type
-            b"1010010101100010"  # checksum
             + b"1010010101100010"
             + b"1010010101100010"
             + b"1010010101100010"
             + b"1010010101100010"  # server's public key
         )
     )
-    session_manager.handle(Packet(b"010" + b"10100100"))  # type, checksum
+    session_manager.handle(Packet(b"010"))  # type
     session_manager.handle(
         Packet(
             b"011"  # type
-            + b"1010010101100010"  # checksum
             + b"1010010101100010"  # datagram num
         )
     )
-    session_manager.handle(Packet(b"100" + b"10100100"))  # type, checksum
-    session_manager.handle(Packet(b"101" + b"10100100"))  # type, checksum
+    session_manager.handle(Packet(b"100"))  # type
+    session_manager.handle(Packet(b"101"))  # type
 
 
 if __name__ == "__main__":
