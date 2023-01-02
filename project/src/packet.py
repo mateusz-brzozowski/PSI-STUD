a3e12cc2828fd4157628ee78b0fb1652059e34d9
Mateusz Brzozowski <01158596@pw.edu.pl>
2023-01-02 22:48:55 +0100

    add packet types

--- project/src/packet.py
+++ project/src/packet.py
@@ -13,3 +13,22 @@ class Packet:
 
     def size(self) -> int:
         return len(self._content)
+
+
+packet_type_client = {
+    "initial": b"000",
+    "session_data": b"001",
+    "declaration": b"010",
+    "send": b"011",
+    "error": b"100",
+    "close": b"101"
+}
+
+packet_type_server = {
+    "initial": b"000",
+    "session_data": b"001",
+    "acknowledge": b"010",
+    "receive": b"011",
+    "error": b"100",
+    "close": b"101"
+}
